7384e958727836771de55d89a35e1159d32bb3db
Kkwafo <kkwafoawua@gmail.com>
2025-06-30 17:19:26 -0300

    feature/product-validation-compose-useServer

--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,11 +1,30 @@
-import ProductForm from './ProductForm';
+"use client"
 
-export default function AddProductForm() {
+import { ProductSchema } from '@/src/schema'
+import { toast } from 'react-toastify'
+
+export default function AddProductForm({ children }: { children: React.ReactNode }) {
+  const handleSubmit = async (formData: FormData) => {
+    const data = {
+      name: formData.get('name'),
+      price: formData.get('price'),
+      categoryId: formData.get('categoryId')
+    }
+    const result = ProductSchema.safeParse(data)
+    console.log(result)
+    if (!result.success) {
+      result.error.issues.forEach(issues => {
+        toast.error(issues.message)
+      });
+      return
+    }
+  }
   return (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadown-md max-w-3xl mx-auto">
-      <form action={''}
-        className="space-y-5">
-        <ProductForm />
+      <form action={handleSubmit}
+        className="space-y-5"
+      >
+        {children}
         <input
           type="submit"
           className='bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer'
