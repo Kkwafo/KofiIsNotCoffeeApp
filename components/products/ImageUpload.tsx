3ab85ff2c280a1c15ad8a8089d3bb8ddb0860da5
Kkwafo <kkwafoawua@gmail.com>
2025-06-30 17:54:05 -0300

    feature/integration-claudinary

---
+++ components/products/ImageUpload.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { CldUploadWidget } from 'next-cloudinary'
+import { TbPhotoPlus } from 'react-icons/tb'
+export default function ImageUpload() {
+  return (
+    <CldUploadWidget
+      uploadPreset='sbd7objh'
+      options={{ maxFiles: 1 }}
+      onSuccess={(result, { widget }) => {
+        console.log(result)
+      }}>
+      {({ open }) => (
+        <>
+          <div
+            className='space-y-2'>
+            <label className='text-slate-800'>
+              Imagen Producto
+            </label>
+            <div
+              className='relative cursor-pointer gover:opacity-70 transition p-10 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100'
+              onClick={() => open()}>
+              <TbPhotoPlus
+                size={50}
+              />
+              <p className='text-lg font-semibold'>Agregar Imagen</p>
+            </div>
+          </div>
+        </>
+      )}
+
+    </CldUploadWidget>
+  )
+}
