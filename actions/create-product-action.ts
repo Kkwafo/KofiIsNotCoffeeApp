390e61bbb7ae57457e0feff907975b223a009a76
Kkwafo <kkwafoawua@gmail.com>
2025-06-30 22:10:50 -0300

    feature-fix/show-cloudinary-images

--- actions/create-product-action.ts
+++ actions/create-product-action.ts
@@ -1,4 +1,4 @@
-'user server'
+'use server'
 
 import { prisma } from '@/src/lib/prisma'
 import { ProductSchema } from '@/src/schema'
--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,6 +1,6 @@
 
 import AddProductButton from '@/components/products/AddProductButton'
-import { formatCurrency } from '@/src/utils'
+import { formatCurrency, getImagePath } from '@/src/utils'
 import { Product } from '@prisma/client'
 import Image from 'next/image'
 
@@ -8,12 +8,13 @@ type ProductCardProps = {
   product: Product
 }
 export default function ProductCard({ product }: ProductCardProps) {
+  const imagePath = getImagePath(product.image)
   return (
     <div className="border bg-white">
       <Image
         width={400}
         height={500}
-        src={`/products/${product.image}.jpg`}
+        src={imagePath}
         alt={`Imagen del producto ${product.name}`}
       />
       <div className="p-5">
