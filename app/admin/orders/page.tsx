73c9fa7cbcd9fc13ed265f86132cfc74903cd47c
Kkwafo <kkwafoawua@gmail.com>
2025-07-01 10:20:58 -0300

    using SWR and making api integration route

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,43 +1,24 @@
+"use client"
+import useSWR from 'swr'
 import OrderCard from '@/components/order/OrderCard';
 import Heading from '@/components/ui/Heading';
-import { prisma } from '@/src/lib/prisma';
-import { revalidatePath } from 'next/cache';
+import { OrderWithProducts } from '@/src/types';
 
-async function getPendingOrders() {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true
-        }
-      }
-    }
-  });
-  return orders;
-}
-export default async function OrderPage() {
-  const orders = await getPendingOrders();
-  return (
+const url = '/admin/orders/api'
+const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
+const { data, error, isLoading } = useSWR<OrderWithProducts>(url, fetcher, {
+  refreshInterval: 60000,
+  revalidateOnFocus: false
+
+})
+if (isLoading) return 'Cargando...'
+export default function OrderPage() {
+  if (data) return (
     <>
       <Heading>Administrar Ordenes</Heading>
-      <form
-        action={async () => {
-          "use server"
-          revalidatePath('/admin/orders')
-        }}>
-        <input
-          type="submit"
-          value='Actualizar Ordenes'
-          className='bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer'
-        />
-
-      </form>
-      {orders.length ? (
+      {data.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map(order => (
+          {data.map(order => (
             <OrderCard
               key={order.id}
               order={order} />
