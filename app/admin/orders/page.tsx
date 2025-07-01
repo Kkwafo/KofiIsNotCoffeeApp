4b1b5221bc7f2ccf82db6400d27c264156f386c3
Kkwafo <kkwafoawua@gmail.com>
2025-07-01 10:36:54 -0300

    fix/api-swr

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,32 +1,33 @@
-"use client"
-import useSWR from 'swr'
+"use client";
 import OrderCard from '@/components/order/OrderCard';
 import Heading from '@/components/ui/Heading';
 import { OrderWithProducts } from '@/src/types';
+import useSWR from 'swr';
 
-const url = '/admin/orders/api'
-const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
-const { data, error, isLoading } = useSWR<OrderWithProducts>(url, fetcher, {
-  refreshInterval: 60000,
-  revalidateOnFocus: false
+const url = '/admin/orders/api';
+const fetcher = () => fetch(url).then(res => res.json());
 
-})
-if (isLoading) return 'Cargando...'
 export default function OrderPage() {
-  if (data) return (
+  const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+    refreshInterval: 60000,
+    revalidateOnFocus: false,
+  });
+
+  if (isLoading) return <p>Cargando...</p>;
+  if (error) return <p>Error cargando pedidos.</p>;
+
+  return (
     <>
       <Heading>Administrar Ordenes</Heading>
-      {data.length ? (
+      {data && data.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
           {data.map(order => (
-            <OrderCard
-              key={order.id}
-              order={order} />
+            <OrderCard key={order.id} order={order} />
           ))}
         </div>
       ) : (
         <p>No hay pedidos pendientes</p>
       )}
     </>
-  )
+  );
 }
